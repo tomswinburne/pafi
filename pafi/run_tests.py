393295b99090f7760c1bcaad4cc425c6a2e4d119
tomswinburne <tomswinburne@gmail.com>
2024-10-11 15:40:15 +0200

    testing

--- pafi/run_tests.py
+++ pafi/run_tests.py
@@ -3,7 +3,10 @@ import pytest
 
 def main():
     # Run pytest on the tests directory
-    exit_code = pytest.main(['tests'])
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the path to the tests directory
+    tests_dir = os.path.join(current_dir, 'tests')
+    exit_code = pytest.main([tests_dir])
     sys.exit(exit_code)
 
 if __name__ == "__main__":
